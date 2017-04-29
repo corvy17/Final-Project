c96dfe9f36d5627175842755b9b6ea863ecaaeed
corvy17 <i.lopez.34.118@gmail.com>
2017-04-28 18:04:10 -0700

    Add files via upload

---
+++ player.java
@@ -0,0 +1,97 @@
+
+public class player {
+		String name;
+		int age;
+		int passing;
+		int rushing;
+		int receiving;
+		int kicking;
+		int fieldgoals;
+		int touchdowns;
+		int totalyards;
+		int scoring;
+		
+		public void setName(String n) {
+			name = n;
+		}	
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setAge(int a) {
+			age = a;
+		}
+		
+		public int getAge() {
+			return age;
+		}	
+		
+		public void setPassing(int p) {
+			passing = p;
+		}	
+		
+		public int getPassing() {
+			return passing;
+		}
+		
+		public void setRushing(int r) {
+			rushing = r;
+		}	
+		
+		public int getRushing() {
+			return rushing;
+		}
+		
+		public void setReceiving(int R) {
+			receiving = R;
+		}	
+		
+		public int getReceiving() {
+			return receiving;
+		}
+		
+		public void setKicking(int k) {
+			kicking = k;
+		}	
+		
+		public int getKicking() {
+			return kicking;
+		}
+		
+		public void setFieldGoals(int f) {
+			fieldgoals = f;
+		}	
+		
+		public int getFieldGoals() {
+			return fieldgoals;
+		}
+		
+		public void setTouchDowns(int t) {
+			touchdowns = t;
+		}	
+		
+		public int getTouchDowns() {
+			return touchdowns;
+		}
+		
+		public void setTotalYards(int y) {
+			totalyards = y;
+		}	
+		
+		public int getTotalYards() {
+			return totalyards;
+		}
+		
+		public void setScoring(int s) {
+			scoring = s;
+		}	
+		
+		public int getScoring() {
+			return scoring;
+		}
+		
+		
+		
+		
+}
