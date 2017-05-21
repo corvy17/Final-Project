c4e5d718f0b98c422ab3b972b79f94cd7b25be8b
corvy17 <i.lopez.34.118@gmail.com>
2017-05-21 13:05:01 -0700

    Add files via upload

---
+++ PlayerManager.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class PlayerManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String CreatePlayers[] = {"Kirk Cousins", "Aaron Rodgers", "Phillip Rivers", 
+				"Carson Palmer", "Russell Wilson", "Eli Manning", 
+				"Ben Roethlisberger", "Tom Brady", "Cam Newton", "Colin Kaepernick"};
+
+		
+		System.out.println(Arrays.toString(CreatePlayers));
+
+	}
+
+}
